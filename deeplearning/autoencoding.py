e51a24845a95adf27979d888134b5eae8c9725a9
gragragrao <mathbass.ken531@gmail.com>
2017-08-01 09:48:31 +0900

    [add]auto encoder追加

--- deeplearning/autoencoding.py
+++ deeplearning/autoencoding.py
@@ -5,21 +5,22 @@ rng = np.random.RandomState(1234)
 random_state = 42
 
 # 学習パラメーター
-X_train, X_test, Y_train, Y_test = load_mnist(N=10000)
+X_train, X_test, Y_train, Y_test = load_mnist(N=70000)
+y_test = np.argmax(Y_test, axis=1)
 
 VIS_UNITS = X_train.shape[1]  # = 784
-HIDDEN_UNITS = 400
-LEARNING_RATE = 0.02
+HIDDEN_UNITS = 500
+LEARNING_RATE = 0.1
 CORRUPTION_LEVEL = np.float32(0.3)  # ノイズ
-N_EPOCHS = 10
-N_BATCHES = 100
+N_EPOCHS = 30
+N_BATCHES = 10
 
 
-class AutoEncoder:
+class Autoencoder:
     def __init__(self, vis_dim, hid_dim, W, function=lambda x: x):
         self.W = W
-        self.a = tf.Variable(np.zeros(vis_dim).astype('float32'))
-        self.b = tf.Variable(np.zeros(hid_dim).astype('float32'))
+        self.a = tf.Variable(np.zeros(vis_dim).astype('float32'), name='a')
+        self.b = tf.Variable(np.zeros(hid_dim).astype('float32'), name='b')
         self.function = function
         self.params = [self.W, self.a, self.b]
 
@@ -44,24 +45,34 @@ class AutoEncoder:
 
 class Dense:
     def __init__(self, in_dim, out_dim, function):
-        self.W = tf.Variable(rng.uniform(low=-0.08, high=0.08, size=(in_dim, out_dim)).astype('float32'))
+        self.W = tf.Variable(rng.uniform(low=-0.08, high=0.08, size=(in_dim, out_dim)).astype('float32'), name='W')
         self.b = tf.Variable(np.zeros([out_dim]).astype('float32'))
         self.function = function
         self.params = [self.W, self.b]
 
-        self.auto_encoder = AutoEncoder(in_dim, out_dim, self.W, self.function)
+        self.ae = Autoencoder(in_dim, out_dim, self.W, self.function)
 
     def f_prop(self, x):
         u = tf.matmul(x, self.W) + self.b
-        return self.function(u)
+        self.z = self.function(u)
+        return self.z
 
     def pretrain(self, x, noise):
-        cost, reconst_x = self.auto_encoder.reconst_error(x, noise)
+        cost, reconst_x = self.ae.reconst_error(x, noise)
         return cost, reconst_x
 
 
-def sgd(cost, params, eps=np.float32(LEARNING_RATE)):
+layers = [
+    Dense(VIS_UNITS, HIDDEN_UNITS, tf.nn.sigmoid),
+    Dense(HIDDEN_UNITS, HIDDEN_UNITS, tf.nn.sigmoid),
+    Dense(HIDDEN_UNITS, HIDDEN_UNITS, tf.nn.sigmoid),
+    Dense(HIDDEN_UNITS, 10, tf.nn.softmax)
+]
+
+
+def sgd(cost, params, eps=np.float32(0.1)):
     g_params = tf.gradients(cost, params)
+
     updates = []
     for param, g_param in zip(params, g_params):
         if g_param is not None:
@@ -69,23 +80,18 @@ def sgd(cost, params, eps=np.float32(LEARNING_RATE)):
     return updates
 
 
-layers = [
-    Dense(VIS_UNITS, HIDDEN_UNITS, tf.nn.sigmoid),
-    Dense(HIDDEN_UNITS, HIDDEN_UNITS, tf.nn.sigmoid),
-    Dense(HIDDEN_UNITS, 10, tf.nn.softmax)
-]
-
 X = np.copy(X_train)
 
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
 
+start_time = time.time()
 for l, layer in enumerate(layers[:-1]):
-    corruption_level = np.float(CORRUPTION_LEVEL)
-    n_epochs = N_EPOCHS
-    batch_size = N_BATCHES
+    corruption_level = np.float(0.3)
+    batch_size = 100
     n_batches = X.shape[0] // batch_size
+    n_epochs = 20
 
     x = tf.placeholder(tf.float32)
     noise = tf.placeholder(tf.float32)
@@ -95,7 +101,6 @@ for l, layer in enumerate(layers[:-1]):
     train = sgd(cost, params)
     encode = layer.f_prop(x)
 
-    start_time = time.time()
     for epoch in range(n_epochs):
         X = shuffle(X, random_state=random_state)
         err_all = []
@@ -103,12 +108,9 @@ for l, layer in enumerate(layers[:-1]):
             start = i * batch_size
             end = start + batch_size
 
-            _noise = rng.binomial(size=X_train[start:end].shape, n=1, p=(1 - corruption_level))
-            _, err = sess.run([train, cost], feed_dict={x: X[start:end], noise: _noise})  # 問題点
+            _noise = rng.binomial(size=X[start:end].shape, n=1, p=1 - corruption_level)
+            _, err = sess.run([train, cost], feed_dict={x: X[start:end], noise: _noise})
             err_all.append(err)
-
-        if (epoch + 1) % 10 == 0:
-            print("{}回目が終了。この層の学習開始から{}sが経過".format(epoch + 1, time.time() - start_time))
 
     X = sess.run(encode, feed_dict={x: X})
 
@@ -124,35 +126,34 @@ for i in range(100):
     fig.savefig('./images/autoencoding_weight.png')
 '''
 
-# x = tf.placeholder(tf.float32, [None, 784])
-# t = tf.placeholder(tf.float32, [None, 10])
-#
-#
-# def f_props(layers, x):
-#     params = []
-#     for layer in layers:
-#         x = layer.f_prop(x)
-#         params += layer.params
-#     return x, params
-#
-#
-# y, params = f_props(layers, x)
-#
-# cost = -tf.reduce_mean(tf.reduce_sum(t * tf.log(y), axis=1))
-# updates = sgd(cost, params)
-# train = tf.group(*updates)
-# valid = tf.argmax(y, axis=1)
-#
-# for epoch in range(n_epochs):
-#     X_train, Y_train = shuffle(X_train, Y_train, random_state=random_state)
-#     for i in range(n_batches):
-#         start = i * batch_size
-#         end = start + batch_size
-#         sess.run(train, feed_dict={x: X_train[start:end], t: Y_train[start:end]})
-#     pred_y, valid_cost = sess.run([valid, cost], feed_dict={x: X_test, t: Y_test})
-#
-# during = time.time() - start_time
-# print('function: ', activation_function.__name__, ', f1 score: ',
-#       f1_score(y_test, pred_y, average='macro'), ', time: ', during, 's')
+x = tf.placeholder(tf.float32, [None, 784])
+t = tf.placeholder(tf.float32, [None, 10])
+
+
+def f_props(layers, x):
+    params = []
+    for layer in layers:
+        x = layer.f_prop(x)
+        params += layer.params
+    return x, params
+
+
+y, params = f_props(layers, x)
+
+cost = -tf.reduce_mean(tf.reduce_sum(t * tf.log(y), axis=1))
+updates = sgd(cost, params)
+train = tf.group(*updates)
+valid = tf.argmax(y, axis=1)
+
+for epoch in range(n_epochs):
+    X_train, Y_train = shuffle(X_train, Y_train, random_state=random_state)
+    for i in range(n_batches):
+        start = i * batch_size
+        end = start + batch_size
+        sess.run(train, feed_dict={x: X_train[start:end], t: Y_train[start:end]})
+    pred_y = sess.run(valid, feed_dict={x: X_test})
+
+during = time.time() - start_time
+print('f1 score: ', f1_score(y_test, pred_y, average='macro'), ', time: ', during, 's')
 
 sess.close()
